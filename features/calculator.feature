0067495461adb36a92f96916b574ef31496cfbb1
Navin Keswani <navin@novemberkilo.com>
2011-09-10 14:20:02 +1000

    Adding in @glv's commit that implements sqrt for the sample calculator

--- features/calculator.feature
+++ features/calculator.feature
@@ -1,15 +1,15 @@
 Feature: Tiny Calculator
   In order to avoid silly mistakes
-  As a math idiot 
+  As a math idiot
   I want to have a calculator to run
-  
+
   Background:
     Given I have loaded the sample calculator parser
- 
+
   Scenario Outline: Run some calculations
     When I ask the calculator for the answer to "<input>"
     Then I should see "<result>"
-    
+
     Examples:
       | input          | result |
       | 24             |     24 |
@@ -20,7 +20,8 @@ Feature: Tiny Calculator
       | 24+2*-(3-12)   |     42 |
       | 24+2*-(3-12^2) |    306 |
       | 24+3*4/2 - 5   |     25 |
-  
+      | 6+sqrt(9)      |      9 |
+
   Scenario: Leading whitespace
     When I ask the calculator for the answer to "  2+2"
     Then I should see "4"
@@ -32,18 +33,21 @@ Feature: Tiny Calculator
   Scenario: Odd whitespace characters
     When I ask the calculator for the answer to " \t \n 2 \r\n + \t - \r 2 \n"
     Then I should see "0"
-    
+
   Scenario Outline: Syntax errors
     When I ask the calculator for the answer to "<input>"
     Then I should get the error "<message>"
-    
+
     Examples:
-      | input | message                               |
-      |       | Unexpected end of input               |
-      | 2+    | Unexpected end of input               |
-      | +2    | Unexpected '+'                        |
-      | 2 2   | Unexpected integer (2)                |
-      | 2+(3  | Unexpected end of input; expected ')' |
-      | 2+)3  | Unexpected ')'                        |
-      | 2+*3  | Unexpected '*'                        |
-      | 2-    | Unexpected end of input               |
+      | input   | message                               |
+      |         | Unexpected end of input               |
+      | 2+      | Unexpected end of input               |
+      | +2      | Unexpected '+'                        |
+      | 2 2     | Unexpected integer (2)                |
+      | 2+(3    | Unexpected end of input; expected ')' |
+      | 2+)3    | Unexpected ')'                        |
+      | 2+*3    | Unexpected '*'                        |
+      | 2-      | Unexpected end of input               |
+      | 3-18(4) | Expected a function name              |
+      | sqrt(8  | Missing closing parenthesis           |
+      | foo(8)  | Unrecognized function                 |
--- samples/calculator.rb
+++ samples/calculator.rb
@@ -3,64 +3,82 @@ require 'smithereen'
 
 module SmithereenSamples
   class CalculatorLexer < Smithereen::Lexer
-    def produce_next_token  
+    def produce_next_token
       return nil if i >= length
 
       rest = s[i..-1]
-    
+
       case rest
       when /\A\s+/m
         move $&.size
         produce_next_token
-      when /\A\d+/ then make_token(:integer, $&)
-      when /\A\+/  then make_token(:+,       $&)
-      when /\A-/   then make_token(:-,       $&)
-      when /\A\*/  then make_token(:*,       $&)
-      when /\A\//  then make_token(:/,       $&)
-      when /\A\^/  then make_token(:'^',     $&)
-      when /\A\(/  then make_token(:'(',     $&)
-      when /\A\)/  then make_token(:')',     $&)
+      when /\A\d+/          then make_token(:integer, $&)
+      when /\A\+/           then make_token(:+,       $&)
+      when /\A-/            then make_token(:-,       $&)
+      when /\A\*/           then make_token(:*,       $&)
+      when /\A\//           then make_token(:/,       $&)
+      when /\A\^/           then make_token(:'^',     $&)
+      when /\A\(/           then make_token(:'(',     $&)
+      when /\A\)/           then make_token(:')',     $&)
+      when /\A[[:alpha:]]+/ then make_token(:name,    $&)
       end
     end
   end
-  
+
   class CalculatorGrammar < Smithereen::Grammar
-    
+
     deftoken :integer, 1000 do
       def value
         @value ||= text.to_i
       end
-    
+
       prefix { value }
     end
-  
+
+    deftoken :name, 1000 do
+      def value
+        @value ||= text
+      end
+
+      prefix { value }
+    end
+
     deftoken :+, 10 do
       infix {|left| left + expression(lbp) }
     end
-  
+
     deftoken :*, 20 do
       infix {|left| left * expression(lbp) }
     end
-    
+
     deftoken :/, 20 do
       infix {|left| left / expression(lbp) }
     end
-  
+
     deftoken :-, 10 do
       prefix { - expression(lbp) }
       infix {|left| left - expression(lbp) }
     end
-  
+
     deftoken :'^', 30 do
       infix {|left| left ** expression(lbp - 1) }
     end
-  
-    deftoken :'(', 0 do
+
+    deftoken :'(', 50 do
       prefix do
         expression.tap{ advance_if_looking_at! :')' }
       end
+      infix do |left|
+        raise ::Smithereen::ParseError.new("Expected a function name", left) unless String === left
+        arg = expression(lbp)
+        advance_if_looking_at(:')') or raise ::Smithereen::ParseError.new("Missing closing parenthesis", nil)
+        case left
+        when 'sqrt' then Math.sqrt(arg)
+        else raise ::Smithereen::ParseError.new("Unrecognized function", left)
+        end
+      end
     end
-  
+
     deftoken :')', 0
 
   end
@@ -68,6 +86,6 @@ module SmithereenSamples
   class Calculator < Smithereen::Parser
     def initialize(s)
       super(CalculatorGrammar.new, CalculatorLexer.new(s))
-    end    
+    end
   end
 end
