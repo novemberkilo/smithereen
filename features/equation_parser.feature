0b1f88e1c7f48cc97b11fe3df63043675bf48412
Navin Keswani <navin@novemberkilo.com>
2011-09-10 19:59:24 +1000

    Implemented a function table

--- features/equation_parser.feature
+++ features/equation_parser.feature
@@ -12,6 +12,7 @@ Feature: Algebraic Equation Parser
     Examples:
       | input          | result |
       | 24             |     24 |
+      | pi             | 3.141592653589793 |
       | 24 +2          |     26 |
       | 24 + 2 * 3     |     30 |
       | 24+2* - 3-12   |      6 |
@@ -19,9 +20,11 @@ Feature: Algebraic Equation Parser
       | 24+2*-(3-12)   |     42 |
       | 24+2*-(3-12^2) |    306 |
       | 24+3*4/2 - 5   |     25 |
-      | sin(pi/2)      |      1 |
+      | 6+sqrt(9)      |      9 |
+      | sin(0)         |      0 |
+      | cos(0)         |      1 |
       | cos(pi)        |     -1 |
-      | sin(2pi)       |      0 |
+      | cos( 3.141592653589793)      |     -1 |
 
   Scenario: Leading whitespace
     When I ask the algebraic equation parser for the answer to "  2+2"
@@ -40,13 +43,15 @@ Feature: Algebraic Equation Parser
     Then I should get the error "<message>"
 
     Examples:
-      | input | message                               |
-      |       | Unexpected end of input               |
-      | 2+    | Unexpected end of input               |
-      | +2    | Unexpected '+'                        |
-      | 2 2   | Unexpected integer (2)                |
-      | 2+(3  | Unexpected end of input; expected ')' |
-      | 2+)3  | Unexpected ')'                        |
-      | 2+*3  | Unexpected '*'                        |
-      | 2-    | Unexpected end of input               |
-
+      | input   | message                               |
+      |         | Unexpected end of input               |
+      | 2+      | Unexpected end of input               |
+      | +2      | Unexpected '+'                        |
+      | 2 2     | Unexpected integer (2)                |
+      | 2+(3    | Unexpected end of input; expected ')' |
+      | 2+)3    | Unexpected ')'                        |
+      | 2+*3    | Unexpected '*'                        |
+      | 2-      | Unexpected end of input               |
+      | 3-18(4) | Expected a function name              |
+      | sqrt(8  | Missing closing parenthesis           |
+      | foo(8)  | Unrecognized function                 |
--- samples/equation_parser.rb
+++ samples/equation_parser.rb
@@ -12,23 +12,48 @@ module SmithereenSamples
       when /\A\s+/m
         move $&.size
         produce_next_token
-      when /\A\d+/ then make_token(:integer, $&)
-      when /\A\+/  then make_token(:+,       $&)
-      when /\A-/   then make_token(:-,       $&)
-      when /\A\*/  then make_token(:*,       $&)
-      when /\A\//  then make_token(:/,       $&)
-      when /\A\^/  then make_token(:'^',     $&)
-      when /\A\(/  then make_token(:'(',     $&)
-      when /\A\)/  then make_token(:')',     $&)
+      when /\A\d+\.\d+/     then make_token(:decimal, $&)
+      when /\A\d+/          then make_token(:integer, $&)
+      when /\A\+/           then make_token(:+,       $&)
+      when /\A-/            then make_token(:-,       $&)
+      when /\A\*/           then make_token(:*,       $&)
+      when /\A\//           then make_token(:/,       $&)
+      when /\A\^/           then make_token(:'^',     $&)
+      when /\A\(/           then make_token(:'(',     $&)
+      when /\A\)/           then make_token(:')',     $&)
+      when /\A[[:alpha:]]+/ then make_token(:name,    $&)
       end
     end
   end
 
   class EquationParserGrammar < Smithereen::Grammar
 
+
+    fns = {
+      'sin' => Proc.new { |x| Math.sin(x) },
+      'cos' => Proc.new { |x| Math.cos(x) },
+      'sqrt' => Proc.new { |x| Math.sqrt(x) }
+    }
+
+    deftoken :decimal, 1000 do
+      def value
+        @value ||= text.to_f
+      end
+
+      prefix { value }
+    end
+
     deftoken :integer, 1000 do
       def value
         @value ||= text.to_i
+      end
+
+      prefix { value }
+    end
+
+    deftoken :name, 1000 do
+      def value
+        @value ||= text
       end
 
       prefix { value }
@@ -55,9 +80,19 @@ module SmithereenSamples
       infix {|left| left ** expression(lbp - 1) }
     end
 
-    deftoken :'(', 0 do
+    deftoken :'(', 50 do
       prefix do
         expression.tap{ advance_if_looking_at! :')' }
+      end
+      infix do |left|
+        raise ::Smithereen::ParseError.new("Expected a function name", left) unless String === left
+        arg = expression(lbp)
+        advance_if_looking_at(:')') or raise ::Smithereen::ParseError.new("Missing closing parenthesis", nil)
+        if fns.keys.include? left
+          fns[left].call(arg)
+        else
+          raise ::Smithereen::ParseError.new("Unrecognized function", left)
+        end
       end
     end
 
